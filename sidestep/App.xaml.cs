7af1dd387975096aecae63b183507bba597135ae
Arthur Thomas <acthomasiv@gmail.com>
2023-01-26 11:55:03 -0500

    Add project files.

---
+++ sidestep/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace sidestep;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ sidestep/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace sidestep;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

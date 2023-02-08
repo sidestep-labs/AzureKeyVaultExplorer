63d7b86f7d9928c905260b3ecdf48e3d1408fa2f
Arthur Thomas <athomas@sandscap.com>
2023-02-08 01:09:53 -0500

    wip

--- sidestep.quickey/Services/AuthService.cs
+++ sidestep.quickey/Services/AuthService.cs
@@ -119,12 +119,10 @@ public class AuthService
             WebAuthenticatorResult authResult = await WebAuthenticator.Default.AuthenticateAsync(
                 new WebAuthenticatorOptions()
                 {
-
+                    //https://youtu.be/gQoqg4P-uJ0?t=129
                     Url = new Uri("https://login.microsoftonline.com/common/oauth2/nativeclient"),
                     CallbackUrl = new Uri($"myapp://"),
 
-                    //Url = new Uri("io.identitymodel.native://callback"),
-                    //CallbackUrl = new Uri("https://demo.identityserver.io"),
                     //PrefersEphemeralWebBrowserSession = true
                 });
 

1fa8576855d0ccececcee4b267bbbca311afafd8
Arthur Thomas <athomas@sandscap.com>
2023-02-14 23:56:01 -0500

    wip title bar

--- sidestep.quickey/MainPage.xaml.cs
+++ sidestep.quickey/MainPage.xaml.cs
@@ -27,26 +27,11 @@ public partial class MainPage : ContentPage
         SemanticScreenReader.Announce(CounterBtn.Text);
     }
 
-
-    protected override void OnHandlerChanged()
-    {
-        base.OnHandlerChanged();
-//#if WINDOWS
-//        var uiSettings = new Windows.UI.ViewManagement.UISettings();
-//        var color = uiSettings.GetColorValue(Windows.UI.ViewManagement.UIColorType.AccentDark2);
-//        Microsoft.UI.Xaml.Window window = (Microsoft.UI.Xaml.Window)App.Current.Windows.First<Window>().Handler.PlatformView;
-//        IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
-//        Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-//        Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-//        Microsoft.UI.Windowing.AppWindowTitleBar titlebar = appWindow.TitleBar;
-//        titlebar.BackgroundColor = color;
-//        titlebar.ButtonBackgroundColor = color;
-//#endif
-    }
-
     private void newWindow_Clicked(object sender, EventArgs e)
     {
         Window secondWindow = new Window(new MainPage());
+        secondWindow.Width = 400;
+        secondWindow.Height = 500;
         Application.Current.OpenWindow(secondWindow);
     }
 }

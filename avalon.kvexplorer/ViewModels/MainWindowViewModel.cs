8920736c39fdc94b249a03cf144563dd43a548b1
Arthur Thomas <athomas@sandscap.com>
2023-03-26 09:33:40 -0400

    wip

--- avalon.kvexplorer/ViewModels/MainWindowViewModel.cs
+++ avalon.kvexplorer/ViewModels/MainWindowViewModel.cs
@@ -3,17 +3,22 @@ using CommunityToolkit.Mvvm.Input;
 using avalon.kvexplorer.Services;
 using System.Collections.Generic;
 using System.Threading;
-using System;
+using static avalon.kvexplorer.ViewModels.MainWindowViewModel;
 
 namespace avalon.kvexplorer.ViewModels;
 
-public partial class WelcomePageViewModel : PageViewModelBase
+public partial class MainWindowViewModel : ViewModelBase
 {
     private readonly AuthService _authService;
-    public WelcomePageViewModel(AuthService authService)
+
+
+    [ObservableProperty]
+    public List<MyData> listOfPeople;
+
+    public MainWindowViewModel(AuthService authService)
     {
         _authService = authService;
-        Data = new List<MyData>
+        listOfPeople = new List<MyData>
         {
             new MyData { Name = "John Doe", Age = 42, Address = "123 Main St." },
             new MyData { Name = "Jane Doe", Age = 39, Address = "456 Oak St." },
@@ -21,41 +26,16 @@ public partial class WelcomePageViewModel : PageViewModelBase
         };
     }
 
-    public WelcomePageViewModel()
+    public MainWindowViewModel()
     {
         _authService = new AuthService();
-        Data = new List<MyData>
+        listOfPeople = new List<MyData>
         {
             new MyData { Name = "John Doe", Age = 42, Address = "123 Main St." },
             new MyData { Name = "Jane Doe", Age = 39, Address = "456 Oak St." },
             new MyData { Name = "Bob Smith", Age = 27, Address = "789 Elm St." }
         };
     }
-
-    /// <summary>
-    /// The Title of this page
-    /// </summary>
-    public string Title => "Welcome to our Wizard-Sample.";
-
-    /// <summary>
-    /// The content of this page
-    /// </summary>
-    public string Message => "Press \"Next\" to register yourself.";
-
-    // This is our first page, so we can navigate to the next page in any case
-    public override bool CanNavigateNext
-    {
-        get => true;
-        protected set => throw new NotSupportedException();
-    }
-
-    // You cannot go back from this page
-    public override bool CanNavigatePrevious
-    {
-        get => false;
-        protected set => throw new NotSupportedException();
-    }
-
 
     public class MyData
     {
@@ -64,16 +44,14 @@ public partial class WelcomePageViewModel : PageViewModelBase
         public string Address { get; set; }
     }
 
-    [ObservableProperty]
-    public List<MyData> _data;
-
+ 
 
     [RelayCommand]
     private async void Login()
     {
         var cancellation = new CancellationToken();
         var account = await _authService.RefreshTokenAsync(cancellation);
-        if(account == null)
+        if (account == null)
         {
             await _authService.LoginAsync(cancellation);
         }
--- avalon.kvexplorer/ViewModels/WelcomePageViewModel.cs
+++ avalon.kvexplorer/ViewModels/WelcomePageViewModel.cs
@@ -3,56 +3,47 @@ using CommunityToolkit.Mvvm.Input;
 using avalon.kvexplorer.Services;
 using System.Collections.Generic;
 using System.Threading;
+using System;
 
 namespace avalon.kvexplorer.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase
+public partial class WelcomePageViewModel : PageViewModelBase
 {
     private readonly AuthService _authService;
-    public MainWindowViewModel(AuthService authService)
+    public WelcomePageViewModel(AuthService authService)
     {
         _authService = authService;
-        Data = new List<MyData>
-        {
-            new MyData { Name = "John Doe", Age = 42, Address = "123 Main St." },
-            new MyData { Name = "Jane Doe", Age = 39, Address = "456 Oak St." },
-            new MyData { Name = "Bob Smith", Age = 27, Address = "789 Elm St." }
-        };
+     
     }
 
-    public MainWindowViewModel()
+    public WelcomePageViewModel()
     {
         _authService = new AuthService();
-        Data = new List<MyData>
-        {
-            new MyData { Name = "John Doe", Age = 42, Address = "123 Main St." },
-            new MyData { Name = "Jane Doe", Age = 39, Address = "456 Oak St." },
-            new MyData { Name = "Bob Smith", Age = 27, Address = "789 Elm St." }
-        };
     }
+    /// <summary>
+    /// The Title of this page
+    /// </summary>
+    public string Title => "Welcome to our Wizard-Sample.";
 
-    public class MyData
+    /// <summary>
+    /// The content of this page
+    /// </summary>
+    public string Message => "Press \"Next\" to register yourself.";
+
+    // This is our first page, so we can navigate to the next page in any case
+    public override bool CanNavigateNext
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Address { get; set; }
+        get => true;
+        protected set => throw new NotSupportedException();
     }
 
-    [ObservableProperty]
-    public List<MyData> _data;
-
-
-    [RelayCommand]
-    private async void Login()
+    // You cannot go back from this page
+    public override bool CanNavigatePrevious
     {
-        var cancellation = new CancellationToken();
-        var account = await _authService.RefreshTokenAsync(cancellation);
-        if(account == null)
-        {
-            await _authService.LoginAsync(cancellation);
-        }
-
+        get => false;
+        protected set => throw new NotSupportedException();
     }
 
-    public string Greeting => "Welcome to Avalonia!";
+
+
 }

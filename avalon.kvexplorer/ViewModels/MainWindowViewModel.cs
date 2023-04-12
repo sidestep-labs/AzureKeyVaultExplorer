96a30d9351f7ff86a5d3aa052d3ef31067535b52
Arthur Thomas <athomas@sandscap.com>
2023-04-11 21:34:47 -0400

    wip

--- avalon.kvexplorer/ViewModels/MainWindowViewModel.cs
+++ avalon.kvexplorer/ViewModels/MainWindowViewModel.cs
@@ -29,14 +29,18 @@ public partial class MainWindowViewModel : ViewModelBase
         _authService = authService;
         _vaultService = vaultService;
         vaultList = new List<KeyVaultResource>();
-        vaultTreeList = new ObservableCollection<KeyVaultModel>();
+        //vaultTreeList = new ObservableCollection<KeyVaultModel>();
 
-        //vaultTreeList = new List<KeyVaultModel>
-        //{
-        //    new KeyVaultModel { SubscriptionDisplayName = "test1", SubscriptionId = "123" },
-        //    new KeyVaultModel { SubscriptionDisplayName = "test2", SubscriptionId = "123" },
+        vaultTreeList = new ObservableCollection<KeyVaultModel>
+        {
+            new KeyVaultModel {
+                SubscriptionDisplayName = "Sandbox Subscription",
+                SubscriptionId = "123",
+                KeyVaultResources = new List<KeyVaultResource>{ } },
 
-        //    };
+            new KeyVaultModel { SubscriptionDisplayName = "Development", SubscriptionId = "123" },
+            new KeyVaultModel { SubscriptionDisplayName = "QA", SubscriptionId = "123" },
+        };
 
         listOfPeople = new List<MyData>
         {
@@ -47,12 +51,26 @@ public partial class MainWindowViewModel : ViewModelBase
 
         Task.Run(async () =>
         {
-            GetAvailableKeyVaultsxx();
+            GetAvailableKeyVaults();
         });
     }
 
-    public MainWindowViewModel() { }
+    public MainWindowViewModel()
+    {
+        vaultTreeList = new ObservableCollection<KeyVaultModel>
+        {
+            new KeyVaultModel { SubscriptionDisplayName = "Sandbox Subscription", SubscriptionId = "1" },
+            new KeyVaultModel { SubscriptionDisplayName = "Development", SubscriptionId = "2" },
+            new KeyVaultModel { SubscriptionDisplayName = "QA", SubscriptionId = "3" },
+        };
 
+        listOfPeople = new List<MyData>
+        {
+            new MyData { Name = "John Doe", Age = 42, Address = "123 Main St." },
+            new MyData { Name = "Jane Doe", Age = 39, Address = "456 Oak St." },
+            new MyData { Name = "Bob Smith", Age = 27, Address = "789 Elm St." }
+        };
+    }
 
     [RelayCommand]
     private async void Login()
@@ -60,40 +78,17 @@ public partial class MainWindowViewModel : ViewModelBase
         var cancellation = new CancellationToken();
         var account = await _authService.RefreshTokenAsync(cancellation);
         if (account == null)
-        {
             await _authService.LoginAsync(cancellation);
-        }
-        var resource = _vaultService.GetKeyVaultResourceBySubscriptionAndResourceGroup();
-        await foreach (var item in resource)
-        {
-            vaultTreeList.Add(item);
-        }
     }
 
     [RelayCommand]
     private async void GetAvailableKeyVaults()
     {
-        var cancellation = new CancellationToken();
-        var account = await _authService.RefreshTokenAsync(cancellation);
-        if (account == null)
-            await _authService.LoginAsync(cancellation);
+        Login();
         var resource = _vaultService.GetKeyVaultResourceBySubscriptionAndResourceGroup();
         await foreach (var item in resource)
         {
-            vaultTreeList.Add(item);
-        }
-    }
-
-    private async void GetAvailableKeyVaultsxx()
-    {
-        var cancellation = new CancellationToken();
-        var account = await _authService.RefreshTokenAsync(cancellation);
-        if (account == null)
-            await _authService.LoginAsync(cancellation);
-        var resource = _vaultService.GetKeyVaultResourceBySubscriptionAndResourceGroup();
-        await foreach (var item in resource)
-        {
-            vaultTreeList.Add(item);
+            VaultTreeList.Add(item);
         }
     }
 

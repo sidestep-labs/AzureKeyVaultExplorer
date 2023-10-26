0b729510526c5da802ef3a6d4b77ad9ec74c80b8
Arthur Thomas <athomas@sandscap.com>
2023-10-25 22:00:15 -0400

    wip

--- avalonia.kvexplorer/Views/CustomControls/KeyVaultTreeList.axaml.cs
+++ avalonia.kvexplorer/Views/CustomControls/KeyVaultTreeList.axaml.cs
@@ -1,16 +1,20 @@
 ﻿using avalonia.kvexplorer.ViewModels;
 using avalonia.kvexplorer.Views.Pages;
+using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Threading;
 using Azure.ResourceManager.KeyVault;
+using FluentAvalonia.Core;
 using FluentAvalonia.UI.Controls;
 using kvexplorer.shared;
 using kvexplorer.shared.Models;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 
 namespace avalonia.kvexplorer.Views.CustomControls;
 
@@ -47,13 +51,46 @@ public partial class KeyVaultTreeList : UserControl
     {
         var tv = sender as TreeView;
         if(tv.SelectedItem is not null)
-            ShowMenu(true);
+        {
+            var kvm = tv.ItemsSource.ElementAt(0) as KeyVaultModel;
+            var showUnpin = kvm.KeyVaultResources.Contains(tv.SelectedItem as KeyVaultResource);
+            ShowMenu(isTransient: true, isCurrentlyPinned: showUnpin);
+        }
         e.Handled = true;
     }
 
-    private void ShowMenu(bool isTransient)
+    private void ShowMenu(bool isTransient, bool isCurrentlyPinned)
     {
         var flyout = Resources["FAMenuFlyoutSubscriptionTreeView"] as FAMenuFlyout;
+
+        // if pinned, enable unpinned item
+        foreach(MenuFlyoutItem item in flyout.Items)
+        {
+            _ = item.Name switch
+            {
+                "PinToQuickAccess" => item.IsVisible = !isCurrentlyPinned,
+                "UnpinFromQuickAccess" => item.IsVisible = isCurrentlyPinned,
+                _ => item.IsVisible = true
+            };
+        }
+        //var unpinnedItem = (MenuFlyoutItem)flyout.Items.ElementAt(1);
+        //unpinnedItem.
+        /*
+                 <ui:MenuFlyoutItem
+                Name="PinToQuickAccess"
+                Command="{Binding PinVaultToQuickAccessCommand}"
+                CommandParameter="{Binding #SubscriptionTreeViewList.SelectedItem}"
+                IconSource="OutlineStar"
+                Text="Pin to Quick Access" />*/
+        //var unpinItem = new MenuFlyoutItem()
+        //{
+        //    Name = "UnPinToQuickAccess",
+        //    IsEnabled = false,
+        //    Text = "Remove From Quick Access",
+        //    IconSource = new SymbolIconSource { Symbol = Symbol.StarOff },
+        //    Command = (DataContext as KeyVaultTreeListViewModel).PinVaultToQuickAccessCommand
+        //};
+        //flyout.Items.Add(unpinItem);
         flyout.ShowMode = isTransient ? FlyoutShowMode.Transient : FlyoutShowMode.Standard;
         var loc = this.FindControl<TreeView>("SubscriptionTreeViewList");
         flyout.ShowAt(loc);

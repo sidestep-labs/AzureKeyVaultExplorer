70be46281838b52e86e83b3e5abedb8a3d7d1181
Arthur Thomas <athomas@sandscap.com>
2023-11-04 12:26:16 -0400

    wip: add dynamic background

--- avalonia.kvexplorer/ViewModels/VaultPageViewModel.cs
+++ avalonia.kvexplorer/ViewModels/VaultPageViewModel.cs
@@ -31,241 +31,240 @@ namespace avalonia.kvexplorer.ViewModels;
 
 public partial class VaultPageViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    public bool isBusy = true;
+	[ObservableProperty]
+	public bool isBusy = true;
 
-    [ObservableProperty]
-    public bool isCertificatesChecked = true;
+	[ObservableProperty]
+	public bool isCertificatesChecked = true;
 
-    [ObservableProperty]
-    public bool isKeysChecked = true;
+	[ObservableProperty]
+	public bool isKeysChecked = true;
 
-    [ObservableProperty]
-    public bool isSecretsChecked = true;
+	[ObservableProperty]
+	public bool isSecretsChecked = true;
 
-    [ObservableProperty]
-    public string searchQuery;
+	[ObservableProperty]
+	public string searchQuery;
 
-    [ObservableProperty]
-    public ObservableCollection<KeyVaultContentsAmalgamation> vaultContents;
+	[ObservableProperty]
+	public ObservableCollection<KeyVaultContentsAmalgamation> vaultContents;
 
-    private readonly VaultService _vaultService;
+	private readonly VaultService _vaultService;
 
-    public VaultPageViewModel()
-    {
-        _vaultService = Defaults.Locator.GetRequiredService<VaultService>();
+	public VaultPageViewModel()
+	{
+		_vaultService = Defaults.Locator.GetRequiredService<VaultService>();
 
-        vaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>() { };
+		vaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>() { };
 
-        for (int i = 0; i < 50; i++)
-        {
-            var sp = (new SecretProperties($"{i}_Demo__Key_Token") { ContentType = "application/json", Enabled = true, ExpiresOn = new System.DateTime(), });
+		for (int i = 0; i < 50; i++)
+		{
+			var sp = (new SecretProperties($"{i}_Demo__Key_Token") { ContentType = "application/json", Enabled = true, ExpiresOn = new System.DateTime(), });
 
-            switch (i % 3)
-            {
-                case 0:
-                    VaultContents.Add(new KeyVaultContentsAmalgamation
-                    {
-                        Name = $"{i}_Secret",
-                        Id = new Uri("https://stackoverflow.com/"),
-                        Type = KeyVaultItemType.Secret,
-                        ContentType = "application/json",
-                        VaultUri = new Uri("https://stackoverflow.com/"),
-                        Version = "version 1",
-                        SecretProperties = sp,
-                    });
-                    break;
+			switch (i % 3)
+			{
+				case 0:
+					VaultContents.Add(new KeyVaultContentsAmalgamation
+					{
+						Name = $"{i}_Secret",
+						Id = new Uri("https://stackoverflow.com/"),
+						Type = KeyVaultItemType.Secret,
+						ContentType = "application/json",
+						VaultUri = new Uri("https://stackoverflow.com/"),
+						Version = "version 1",
+						SecretProperties = sp,
+					});
+					break;
 
-                case 1:
-                    VaultContents.Add(new KeyVaultContentsAmalgamation
-                    {
-                        Name = $"{i}__Key",
-                        Id = new Uri("https://stackoverflow.com/"),
-                        Type = KeyVaultItemType.Key,
-                        ContentType = "application/json",
-                        VaultUri = new Uri("https://stackoverflow.com/"),
-                        Version = "version 1",
-                        SecretProperties = sp,
-                    });
-                    break;
+				case 1:
+					VaultContents.Add(new KeyVaultContentsAmalgamation
+					{
+						Name = $"{i}__Key",
+						Id = new Uri("https://stackoverflow.com/"),
+						Type = KeyVaultItemType.Key,
+						ContentType = "application/json",
+						VaultUri = new Uri("https://stackoverflow.com/"),
+						Version = "version 1",
+						SecretProperties = sp,
+					});
+					break;
 
-                case 2:
-                    VaultContents.Add(new KeyVaultContentsAmalgamation
-                    {
-                        Name = $"{i}_Certificate",
-                        Id = new Uri("https://stackoverflow.com/"),
-                        Type = KeyVaultItemType.Certificate,
-                        ContentType = "application/json",
-                        VaultUri = new Uri("https://stackoverflow.com/"),
-                        Version = "version 1",
-                        SecretProperties = sp,
-                    });
-                    break;
-            }
-            _vaultContents = VaultContents;
-        }
-    }
+				case 2:
+					VaultContents.Add(new KeyVaultContentsAmalgamation
+					{
+						Name = $"{i}_Certificate",
+						Id = new Uri("https://stackoverflow.com/"),
+						Type = KeyVaultItemType.Certificate,
+						ContentType = "application/json",
+						VaultUri = new Uri("https://stackoverflow.com/"),
+						Version = "version 1",
+						SecretProperties = sp,
+					});
+					break;
+			}
+			_vaultContents = VaultContents;
+		}
+	}
 
-    public Dictionary<KeyVaultItemType, bool> CheckedBoxes { get; set; } = new Dictionary<KeyVaultItemType, bool>() {
-          { KeyVaultItemType.Key, true},
-          { KeyVaultItemType.Secret, true},
-          { KeyVaultItemType.Certificate, true},
-    };
+	public Dictionary<KeyVaultItemType, bool> CheckedBoxes { get; set; } = new Dictionary<KeyVaultItemType, bool>() {
+		  { KeyVaultItemType.Key, true},
+		  { KeyVaultItemType.Secret, true},
+		  { KeyVaultItemType.Certificate, true},
+	};
 
-    private IEnumerable<KeyVaultContentsAmalgamation> _vaultContents { get; set; }
+	private IEnumerable<KeyVaultContentsAmalgamation> _vaultContents { get; set; }
 
-    /*
-     *
-      get => new DataGridCollectionView(VaultContents)
-        {
-            GroupDescriptions = { new DataGridPathGroupDescription("Name") }
-        }
-     */
-    //    public async Task<IEnumerable<KeyVaultContentsAmalgamation>> GetSecretsForVault(Uri kvUri)
+	/*
+	 *
+	  get => new DataGridCollectionView(VaultContents)
+		{
+			GroupDescriptions = { new DataGridPathGroupDescription("Name") }
+		}
+	 */
+	//    public async Task<IEnumerable<KeyVaultContentsAmalgamation>> GetSecretsForVault(Uri kvUri)
 
-    public void FilterBasedOnCheckedBoxes()
-    {
-        var toFilter = CheckedBoxes.Where(v => v.Value == true).Select(s => s.Key).ToList();
-        VaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>(_vaultContents.Where(v => toFilter.Contains(v.Type) && v.Name.ToLowerInvariant().Contains(SearchQuery ?? "")));
-    }
+	public void FilterBasedOnCheckedBoxes()
+	{
+		var toFilter = CheckedBoxes.Where(v => v.Value == true).Select(s => s.Key).ToList();
+		VaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>(_vaultContents.Where(v => toFilter.Contains(v.Type) && v.Name.ToLowerInvariant().Contains(SearchQuery ?? "")));
+	}
 
-    public async Task GetSecretsForVault(Uri kvUri)
-    {
-        var values = _vaultService.GetVaultAssociatedSecrets(kvUri);
-        await foreach (var secret in values)
-        {
-            VaultContents.Add(new KeyVaultContentsAmalgamation
-            {
-                Name = secret.Name,
-                Id = secret.Id,
-                Type = KeyVaultItemType.Secret,
-                ContentType = secret.ContentType,
-                VaultUri = secret.VaultUri,
-                Version = secret.Version,
-                SecretProperties = secret,
-            });
-        }
+	public async Task GetSecretsForVault(Uri kvUri)
+	{
+		var values = _vaultService.GetVaultAssociatedSecrets(kvUri);
+		await foreach (var secret in values)
+		{
+			VaultContents.Add(new KeyVaultContentsAmalgamation
+			{
+				Name = secret.Name,
+				Id = secret.Id,
+				Type = KeyVaultItemType.Secret,
+				ContentType = secret.ContentType,
+				VaultUri = secret.VaultUri,
+				Version = secret.Version,
+				SecretProperties = secret,
+			});
+		}
 
-        var certs = _vaultService.GetVaultAssociatedCertificates(kvUri);
-        await foreach (var cert in certs)
-        {
-            VaultContents.Add(new KeyVaultContentsAmalgamation
-            {
-                Name = cert.Name,
-                Id = cert.Id,
-                Type = KeyVaultItemType.Certificate,
-                VaultUri = cert.VaultUri,
-                Version = cert.Version,
-                CertificateProperties = cert,
-            });
-        }
-        var keys = _vaultService.GetVaultAssociatedKeys(kvUri);
-        await foreach (var key in keys)
-        {
-            VaultContents.Add(new KeyVaultContentsAmalgamation
-            {
-                Name = key.Name,
-                Id = key.Id,
-                Type = KeyVaultItemType.Key,
-                VaultUri = key.VaultUri,
-                Version = key.Version,
-                KeyProperties = key,
-            });
-        }
-        _vaultContents = VaultContents;
+		var certs = _vaultService.GetVaultAssociatedCertificates(kvUri);
+		await foreach (var cert in certs)
+		{
+			VaultContents.Add(new KeyVaultContentsAmalgamation
+			{
+				Name = cert.Name,
+				Id = cert.Id,
+				Type = KeyVaultItemType.Certificate,
+				VaultUri = cert.VaultUri,
+				Version = cert.Version,
+				CertificateProperties = cert,
+			});
+		}
+		var keys = _vaultService.GetVaultAssociatedKeys(kvUri);
+		await foreach (var key in keys)
+		{
+			VaultContents.Add(new KeyVaultContentsAmalgamation
+			{
+				Name = key.Name,
+				Id = key.Id,
+				Type = KeyVaultItemType.Key,
+				VaultUri = key.VaultUri,
+				Version = key.Version,
+				KeyProperties = key,
+			});
+		}
+		_vaultContents = VaultContents;
 
-        //return _vaultContents;
-    }
+		//return _vaultContents;
+	}
 
-    partial void OnIsCertificatesCheckedChanged(bool value) => CheckedBoxes[KeyVaultItemType.Certificate] = value;
+	partial void OnIsCertificatesCheckedChanged(bool value) => CheckedBoxes[KeyVaultItemType.Certificate] = value;
 
-    partial void OnIsKeysCheckedChanged(bool value)
-    {
-        CheckedBoxes[KeyVaultItemType.Key] = value;
+	partial void OnIsKeysCheckedChanged(bool value)
+	{
+		CheckedBoxes[KeyVaultItemType.Key] = value;
 
-        //Dispatcher.UIThread.Post(() => FilterBasedOnCheckedBoxes(), DispatcherPriority.Input);
-    }
+		//Dispatcher.UIThread.Post(() => FilterBasedOnCheckedBoxes(), DispatcherPriority.Input);
+	}
 
-    partial void OnIsSecretsCheckedChanged(bool value)
-    {
-        CheckedBoxes[KeyVaultItemType.Secret] = value;
-    }
+	partial void OnIsSecretsCheckedChanged(bool value)
+	{
+		CheckedBoxes[KeyVaultItemType.Secret] = value;
+	}
 
-    partial void OnSearchQueryChanged(string value)
-    {
-        string query = value?.Trim().ToLowerInvariant();
-        if (!string.IsNullOrWhiteSpace(query))
-        {
-            VaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>(_vaultContents);
-        }
-        var toFilter = CheckedBoxes.Where(v => v.Value == true).Select(s => s.Key).ToList();
-        var list = _vaultContents.Where(v => v.Name.ToLowerInvariant().Contains(query) && toFilter.Contains(v.Type));
-        VaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>(list);
-    }
+	partial void OnSearchQueryChanged(string value)
+	{
+		string query = value?.Trim().ToLowerInvariant();
+		if (!string.IsNullOrWhiteSpace(query))
+		{
+			VaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>(_vaultContents);
+		}
+		var toFilter = CheckedBoxes.Where(v => v.Value == true).Select(s => s.Key).ToList();
+		var list = _vaultContents.Where(v => v.Name.ToLowerInvariant().Contains(query) && toFilter.Contains(v.Type));
+		VaultContents = new ObservableCollection<KeyVaultContentsAmalgamation>(list);
+	}
 
-    private readonly INotificationManager _notificationManager;
+	private readonly INotificationManager _notificationManager;
 
-    [RelayCommand]
-    private async Task Copy(KeyVaultContentsAmalgamation keyVaultItem)
-    {
-        if (keyVaultItem is null) return;
+	[RelayCommand]
+	private async Task Copy(KeyVaultContentsAmalgamation keyVaultItem)
+	{
+		if (keyVaultItem is null) return;
 
-        var topLevel = (Avalonia.Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime).MainWindow;
-        var clipboard = TopLevel.GetTopLevel(topLevel)?.Clipboard;
-        Notification notif;
-        try
-        {
-            string value = string.Empty;
-            //_ = keyVaultItem.Type switch
-            //{
-            //    KeyVaultItemType.Key => value = (await _vaultService.GetKey(keyVaultItem.VaultUri, keyVaultItem.Name)).Key.ToRSA().ToXmlString(true),
-            //    KeyVaultItemType.Secret => value = (await _vaultService.GetSecret(keyVaultItem.VaultUri, keyVaultItem.Name)).Value,
-            //    KeyVaultItemType.Certificate => value = (await _vaultService.GetCertificate(keyVaultItem.VaultUri, keyVaultItem.Name)).Name,
-            //    _ => throw new NotImplementedException()
-            //};
+		var topLevel = (Avalonia.Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime).MainWindow;
+		var clipboard = TopLevel.GetTopLevel(topLevel)?.Clipboard;
+		Notification notif;
+		try
+		{
+			string value = string.Empty;
+			//_ = keyVaultItem.Type switch
+			//{
+			//    KeyVaultItemType.Key => value = (await _vaultService.GetKey(keyVaultItem.VaultUri, keyVaultItem.Name)).Key.ToRSA().ToXmlString(true),
+			//    KeyVaultItemType.Secret => value = (await _vaultService.GetSecret(keyVaultItem.VaultUri, keyVaultItem.Name)).Value,
+			//    KeyVaultItemType.Certificate => value = (await _vaultService.GetCertificate(keyVaultItem.VaultUri, keyVaultItem.Name)).Name,
+			//    _ => throw new NotImplementedException()
+			//};
 
-            if (keyVaultItem.Type == KeyVaultItemType.Key)
-            {
-                var key = await _vaultService.GetKey(keyVaultItem.VaultUri, keyVaultItem.Name);
+			if (keyVaultItem.Type == KeyVaultItemType.Key)
+			{
+				var key = await _vaultService.GetKey(keyVaultItem.VaultUri, keyVaultItem.Name);
+				if (key.KeyType == KeyType.Rsa) {
+					using var rsa = key.Key.ToRSA();
+					var publicKey = rsa.ExportRSAPublicKey();
+					string pem = "-----BEGIN PUBLIC KEY-----\n" + Convert.ToBase64String(publicKey) + "\n-----END PUBLIC KEY-----";
+					value = pem;
+				}
+			}
 
+			if (keyVaultItem.Type == KeyVaultItemType.Secret)
+			{
+				var sv = await _vaultService.GetSecret(keyVaultItem.VaultUri, keyVaultItem.Name);
+				value = sv.Value;
+			}
+			if (keyVaultItem.Type == KeyVaultItemType.Certificate)
+			{
+				var certValue = await _vaultService.GetCertificate(keyVaultItem.VaultUri, keyVaultItem.Name);
 
-                if (key.KeyType == KeyType.Rsa) {
-           
-                    using var rsa = key.Key.ToRSA();
-                    var publicKey = rsa.ExportRSAPublicKey();
-                    string pem = "-----BEGIN PUBLIC KEY-----\n" + Convert.ToBase64String(publicKey) + "\n-----END PUBLIC KEY-----";
-                    value = pem;
-                }
-            }
+			}
 
-            if (keyVaultItem.Type == KeyVaultItemType.Secret)
-            {
-                var sv = await _vaultService.GetSecret(keyVaultItem.VaultUri, keyVaultItem.Name);
-                value = sv.Value;
-            }
-            if (keyVaultItem.Type == KeyVaultItemType.Certificate)
-            {
-            }
+			var dataObject = new DataObject();
+			dataObject.Set(DataFormats.Text, value);
+			await clipboard.SetTextAsync(value);
+			// TODO: figure out why set data object async fails here.
+			notif = new Notification("Copied", $"The value of '{keyVaultItem.Name}' has been copied to the clipboard.", NotificationType.Success);
+		}
+		catch (KeyVaultItemNotFoundException ex)
+		{
+			notif = new Notification($"A value was not found for '{keyVaultItem.Name}'", $"The value of was not able to be retrieved.\n {ex.Message}", NotificationType.Error);
+		}
 
-            var dataObject = new DataObject();
-            dataObject.Set(DataFormats.Text, value);
-            await clipboard.SetTextAsync(value);
-            // TODO: figure out why set data object async fails here.
-            notif = new Notification("Copied", $"The value of '{keyVaultItem.Name}' has been copied to the clipboard.", NotificationType.Success);
-        }
-        catch (KeyVaultItemNotFoundException ex)
-        {
-            notif = new Notification($"A value was not found for '{keyVaultItem.Name}'", $"The value of was not able to be retrieved.\n {ex.Message}", NotificationType.Error);
-        }
-
-        var nm = new WindowNotificationManager(topLevel)
-        {
-            Position = NotificationPosition.BottomRight,
-            MaxItems = 1,
-        };
-        nm.TemplateApplied += (sender, args) =>
-        {
-            nm.Show(notif);
-        };
-    }
+		var nm = new WindowNotificationManager(topLevel)
+		{
+			Position = NotificationPosition.BottomRight,
+			MaxItems = 1,
+		};
+		nm.TemplateApplied += (sender, args) =>
+		{
+			nm.Show(notif);
+		};
+	}
 }
--- avalonia.kvexplorer/Views/MainWindow.axaml.cs
+++ avalonia.kvexplorer/Views/MainWindow.axaml.cs
@@ -3,8 +3,12 @@ using Avalonia.Controls;
 using Avalonia.Controls.Notifications;
 using Avalonia.Interactivity;
 using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Styling;
+using FluentAvalonia.Styling;
 using FluentAvalonia.UI.Windowing;
 using System;
+using System.Diagnostics;
 
 namespace avalonia.kvexplorer.Views;
 
@@ -19,6 +23,15 @@ public partial class MainWindow : AppWindow
         TitleBar.ExtendsContentIntoTitleBar = true;
         TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
 
+        Activated += (sender, e) =>
+        {
+            Background = null;
+        };
+        Deactivated += (sender, e) =>
+        {
+            App.Current.Resources.TryGetResource("DynamicActiveBackgroundFAColor", null, out var bg);
+            Background = (IBrush)bg;
+        };
         //TitleBar.ExtendsContentIntoTitleBar = OperatingSystem.IsMacOS() ? true : false;
         // ExtendClientAreaChromeHints = OperatingSystem.IsMacOS() ? Avalonia.Platform.ExtendClientAreaChromeHints.NoChrome : Avalonia.Platform.ExtendClientAreaChromeHints.Default;
     }

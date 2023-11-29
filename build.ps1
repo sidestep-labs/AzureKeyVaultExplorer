d643210b8aebf3ed7d6dc15d2118ad5d7c4493f6
Arthur Thomas <athomas@sandscap.com>
2023-11-29 00:29:37 -0500

    wip: cleanup

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 param(
     [switch]$RunBuild = $false,
+    [switch]$RunBuildWindows = $false,
     [System.Diagnostics.Stopwatch]$sw = [System.Diagnostics.Stopwatch]::StartNew(),
     [string]$BuildNumber = '1.0.0.0',
     $VersionPrefix = "1.0.0",
@@ -9,11 +10,11 @@ $DebugPreference = 'continue';
 # https://github.com/AvaloniaUI/Avalonia/issues/9503
 if ($RunBuild) {
     
-    Push-Location  C:\repos\sidestep\kvexplorer.Desktop.Windows;
+    Push-Location  C:\repos\sidestep\kvexplorer.Desktop;
 
     $env:KVEXPLORER_APP_VERSION = $BuildNumber
-    dotnet publish  -o publish/ -c Release --self-contained -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix
-       #New-Item -Path $ProjectDir -Name "VERSION" -ItemType "file" -Value $BuildNumber -Force
+    dotnet publish  -o publish/ -c Release --self-contained -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix -f net8.0
+    #New-Item -Path $ProjectDir -Name "VERSION" -ItemType "file" -Value $BuildNumber -Force
 
     explorer.exe .
     
@@ -22,12 +23,36 @@ if ($RunBuild) {
 
     $sw
   
+    Push-Location  c:\repos\sidestep\kvexplorer.Desktop/publish
+
+    .\KeyVaultExplorer.exe 
+
     return;
 }
 
-Push-Location  c:\repos\sidestep\kvexplorer.Desktop/publish
+if ($RunBuildWindows) {
+    
+    Push-Location  C:\repos\sidestep\kvexplorer.Desktop;
 
-.\KeyVaultExplorer.exe 
+    $env:KVEXPLORER_APP_VERSION = $BuildNumber
+    dotnet publish  -o publishWin/ -c Release --self-contained -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix -f net8.0-windows10.0.19041.0 --publish-aot
+    #New-Item -Path $ProjectDir -Name "VERSION" -ItemType "file" -Value $BuildNumber -Force
+
+    explorer.exe .
+    
+    pop-location
+    $sw.Stop()
+
+    $sw
+  
+    Push-Location  c:\repos\sidestep\kvexplorer.Desktop/publish
+
+    .\KeyVaultExplorer.exe 
+
+    return;
+}
+
+
 
 Pop-Location
 $sw.Stop()

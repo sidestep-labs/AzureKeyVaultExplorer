18f518cc82c368fe98d11baeda855c9ad84db954
Arthur Thomas <athomas@sandscap.com>
2023-11-29 22:54:02 -0500

    wip: cleanup

--- build.ps1
+++ build.ps1
@@ -1,63 +1,29 @@
 param(
     [switch]$RunBuild = $false,
-    [switch]$RunBuildWindows = $false,
     [System.Diagnostics.Stopwatch]$sw = [System.Diagnostics.Stopwatch]::StartNew(),
     [string]$BuildNumber = '0.0.1.2',
     $VersionPrefix = "1.0.0",
-    $VersionSuffix = "99"
+    $VersionSuffix = "99",
+    [ValidateSet('net8.0', 'net8.0-windows10.0.19041.0')]
+    [string]$Platform = 'net8.0'
 )
 $DebugPreference = 'continue';
 # https://github.com/AvaloniaUI/Avalonia/issues/9503
 if ($RunBuild) {
-    
     Push-Location  C:\repos\sidestep\kvexplorer.Desktop;
-
     $env:KVEXPLORER_APP_VERSION = $BuildNumber
-    dotnet publish  -o publish/ -c Release --self-contained -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix -f net8.0
+    dotnet publish  -o publish/ -c Release --self-contained -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix -f $Platform
     #New-Item -Path $ProjectDir -Name "VERSION" -ItemType "file" -Value $BuildNumber -Force
-
     explorer.exe .
-    
     pop-location
     $sw.Stop()
-
     $sw
-  
     Push-Location  c:\repos\sidestep\kvexplorer.Desktop/publish
-
     .\KeyVaultExplorer.exe 
-
     return;
 }
-
-if ($RunBuildWindows) {
-    
-    Push-Location  C:\repos\sidestep\kvexplorer.Desktop.Windows;
-
-    $env:KVEXPLORER_APP_VERSION = $BuildNumber
-    dotnet publish  -o publish/ -c Release --self-contained -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix -f net8.0-windows10.0.19041.0
-    #New-Item -Path $ProjectDir -Name "VERSION" -ItemType "file" -Value $BuildNumber -Force
-
-    explorer.exe .
-    
-    pop-location
-    $sw.Stop()
-
-    $sw
-  
-    Push-Location  c:\repos\sidestep\kvexplorer.Desktop.Windows\publish
-
-    # .\'Key Vault Explorer.exe' 
-
-    return;
-}
-
-
-
 Pop-Location
 $sw.Stop()
-
 Write-Debug "App elapsed start up: $($sw.Elapsed)"
-
 $sw
 

4cb907782f4d0402e7a1479e6951e96ff010bcbe
Arthur Thomas <athomas@sandscap.com>
2024-02-15 08:13:22 -0500

    wip

--- kvexplorer/ViewModels/BookmarksPageViewModel.cs
+++ kvexplorer/ViewModels/BookmarksPageViewModel.cs
@@ -14,46 +14,57 @@ namespace kvexplorer.ViewModels;
 
 public partial class BookmarksPageViewModel : ViewModelBase
 {
-    private readonly VaultService _vaultService;
-    private readonly KvExplorerDb _db;
+    [ObservableProperty]
+    public bool isBusy = true;
+
+    [ObservableProperty]
+    public ObservableCollection<SubscriptionResource> selectedSubscriptions;
 
     [ObservableProperty]
     public ObservableCollection<SubscriptionData> subscriptions;
 
+
+    [ObservableProperty]
+    public string continuationToken; 
+
+    private readonly KvExplorerDb _db;
+    private readonly VaultService _vaultService;
     public BookmarksPageViewModel()
     {
         _vaultService = Defaults.Locator.GetRequiredService<VaultService>();
         _db = Defaults.Locator.GetRequiredService<KvExplorerDb>();
-        SelectedSubscription = new();
+        SelectedSubscriptions = new();
         Subscriptions = new ObservableCollection<SubscriptionData>();
+        GetAllKeyVaults().Wait();
     }
-
-   
-
-    [ObservableProperty]
-    public ObservableCollection<SubscriptionResource> selectedSubscription;
-    /// <summary>
-    /// The Title of this page
-    /// </summary>
-    public string Title => "Welcome to our Wizard-Sample.";
-
     /// <summary>
     /// The content of this page
     /// </summary>
     public string Message => "Press \"Next\" to register yourself.";
 
-
+    /// <summary>
+    /// The Title of this page
+    /// </summary>
+    public string Title => "Welcome to our Wizard-Sample.";
     [RelayCommand]
     public async Task GetAllKeyVaults()
     {
+        int count = 0;
         await Dispatcher.UIThread.InvokeAsync(async () =>
         {
             var resource = _vaultService.GetAllSubscriptions();
             await foreach (var item in resource)
             {
                 Subscriptions.Add(item.SubscriptionResource.Data);
-                Debug.WriteLine(item.ContinuationToken);
+                count++;
+                if (item.ContinuationToken != null && count >= 100)
+                {
+                    ContinuationToken = item.ContinuationToken;
+                    Debug.WriteLine(item.ContinuationToken);
+                    break;
+                }
             }
+            IsBusy = false;
         });
     }
 }

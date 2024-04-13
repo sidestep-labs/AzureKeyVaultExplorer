eb1af6e96f4856e0a91e6093ea5e9e8ccc0d0f3c
Arthur Thomas <athomas@sandscap.com>
2024-04-12 22:12:07 -0400

    wip

--- kvexplorer/Views/Pages/TabViewPage.axaml.cs
+++ kvexplorer/Views/Pages/TabViewPage.axaml.cs
@@ -201,7 +201,7 @@ public partial class TabViewPage : UserControl
         //    }
         //}
 
-        //foreach (TabViewItem item in TabViewDoc.TabItems)
+        //foreach (TabViewItem item in TabView.TabItems)
         //{
         //    if (item.IsSelected) {
         //    item.Classes.

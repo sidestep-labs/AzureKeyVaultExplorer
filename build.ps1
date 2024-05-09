2971c69688e6d699739d9cdb876e6478e5d72c33
Arthur Thomas <acthomasiv@gmail.com>
2024-05-09 00:05:15 -0400

    update build resources

--- build.ps1
+++ build.ps1
@@ -11,9 +11,21 @@ param(
 $DebugPreference = 'continue';
 # https://github.com/AvaloniaUI/Avalonia/issues/9503
 $env:KVEXPLORER_APP_VERSION = $BuildNumber
-# // -p:PublishSingleFile=true
-# --self-contained
-dotnet publish  ./KeyVaultExplorer.Desktop/KeyVaultExplorer.Desktop.csproj  --runtime $Runtime  -o .\publish -c Release  -p:VersionPrefix=$VersionPrefix -p:VersionSuffix=$VersionSuffix -f $Platform -p:PublishAot=$PublishAot  -p:PublishReadyToRun=true  -p:PublishTrimmed=true -p:TrimMode=partial -p:IncludeNativeLibrariesForSelfExtract=true -p:PublishSingleFile=$($PublishAot ? "false":"true") --self-contained
+dotnet publish  ./KeyVaultExplorer.Desktop/KeyVaultExplorer.Desktop.csproj `
+    --runtime $Runtime `
+    -o .\publish `
+    -c Release  `
+    -p:VersionPrefix=$VersionPrefix `
+    -p:VersionSuffix=$VersionSuffix `
+    -f $Platform `
+    -p:PublishAot=$PublishAot  `
+    -p:PublishReadyToRun=true  `
+    -p:PublishTrimmed=true `
+    -p:TrimMode=partial `
+    -p:IncludeNativeLibrariesForSelfExtract=true `
+    -p:PublishSingleFile=$($PublishAot ? "false":"true") `
+    -p:UseAppHost=true `
+    --self-contained 
 
 if ($Runtime -match "osx") { 
 

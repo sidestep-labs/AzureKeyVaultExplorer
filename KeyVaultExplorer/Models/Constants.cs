1557f2527a5e941be517c6b8ebd6f3f7571cb051
Arthur Thomas <athomas@sandscap.com>
2024-05-09 21:56:30 -0400

    fix build pipeline

--- KeyVaultExplorer/Models/Constants.cs
+++ KeyVaultExplorer/Models/Constants.cs
@@ -7,41 +7,7 @@ namespace KeyVaultExplorer.Models;
 public static class Constants
 {
     //The Application or Client ID will be generated while registering the app in the Azure portal. Copy and paste the GUID.
-    public static readonly string ClientId = "7c09c1d9-3585-403c-834a-53452958e76f";
-
-    //public static readonly Uri Url = new Uri($"https://login.microsoftonline.com/common/adminconsent?client_id={ClientId}&state=initlogin&redirect_uri=msauth.com.company.KeyVaultExplorer://auth");
-
-    //public static readonly Uri Url = new Uri($"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={ClientId}&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default&grant_type=client_credentials&redirect_uri=msauth.com.company.KeyVaultExplorer://auth");
-
-    public static readonly Uri Url = new($"https://login.microsoftonline.com/958763de-a224-4d6c-8eb1-c63b2fb66f1b/oauth2/v2.0/authorize?" +
-        $"client_id={ClientId}" +
-        $"&response_type=token code" +
-        $"&redirect_uri=msauth.com.company.KeyVaultExplorer://auth" +
-        $"&response_mode=query" +
-        $"&scope=openid offline_access profile email" +
-        $"&state=login" +
-        $"&nonce=111821");
-
-    public static readonly Uri AuthCodeFlowUri = new($"https://login.microsoftonline.com/common/oauth2/v2.0/authorize" +
-        $"?client_id={ClientId}" +
-        $"&response_type=code id_token" +
-        $"&redirect_uri=msauth.com.company.KeyVaultExplorer://auth" +
-        $"&response_mode=fragment" +
-        $"&scope=openid offline_access profile email" +
-        $"&state=1" +
-        $"&nonce=1");
-
-    //$"&code_challenge=YTFjNjI1OWYzMzA3MTI4ZDY2Njg5M2RkNmVjNDE5YmEyZGRhOGYyM2IzNjdmZWFhMTQ1ODg3NDcxY2Nl" +
-    //$"&code_challenge_method=S25
-
-    public static readonly Uri AuthCodeFlowAccessTokenUri = new($"https://login.microsoftonline.com/common/oauth2/v2.0/authorize" +
-           $"?client_id={ClientId}" +
-           $"&response_type=code id_token" +
-           $"&redirect_uri=msauth.com.company.KeyVaultExplorer://auth" +
-           $"&response_mode=fragment" +
-           $"&scope=openid offline_access profile email" +
-           $"&state=1" +
-           $"&nonce=1");
+    public static readonly string ClientId = "fdc1e6da-d735-4627-af3e-d40377f55713";
 
     //Leaving the scope to its default values.
     public static readonly string[] Scopes = ["openid", "offline_access", "profile", "email",];
@@ -53,7 +19,7 @@ public static class Constants
     public static readonly string[] AzureScopes = ["https://management.core.windows.net//.default", "https://vault.azure.net//.default", "user_impersonation"];
 
     // Cache settings
-    public const string CacheFileName = "kvexplorer_msal_cache.txt";
+    public const string CacheFileName = "keyvaultexplorer_msal_cache.txt";
 
     public static readonly string LocalAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\KeyVaultExplorer";
 
@@ -62,7 +28,7 @@ public static class Constants
 
     public const string LinuxKeyRingSchema = "com.sidestep.keyvaultexplorer.tokencache";
     public const string LinuxKeyRingCollection = MsalCacheHelper.LinuxKeyRingDefaultCollection;
-    public const string LinuxKeyRingLabel = "MSAL token cache for all Contoso dev tool apps.";
+    public const string LinuxKeyRingLabel = "MSAL token cache for key vault explorer.";
     public static readonly KeyValuePair<string, string> LinuxKeyRingAttr1 = new KeyValuePair<string, string>("Version", "1");
     public static readonly KeyValuePair<string, string> LinuxKeyRingAttr2 = new KeyValuePair<string, string>("ProductGroup", "MyApps");
 }
